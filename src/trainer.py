42760f548ac4ea4e8431699acb718d370b04a70d
orris27 <gsorris27@gmail.com>
2020-08-16 16:08:12 +0800

    debug

--- src/trainer.py
+++ src/trainer.py
@@ -108,12 +108,16 @@ class Trainer():
                             self.ckp.save_results(d, filename[0], save_list, scale, exit)
 
                 self.ckp.log[-1, idx_data, idx_scale] /= len(d)
-                best = self.ckp.log.max(0)
+                #best = self.ckp.log.max(0)
+                best = self.ckp.log[:, -1, :, :].max(0)
                 self.ckp.write_log(
-                    '[{} x{}]\tPSNR: {:.3f} (Best: {:.3f} @epoch {})'.format(
+                        '[{} x{}]\tPSNR: {:.3f} {:.3f} {:.3f} {:.3f} (Best: {:.3f} @epoch {})'.format(
                         d.dataset.name,
                         scale,
-                        self.ckp.log[-1, idx_data, idx_scale],
+                        self.ckp.log[-1, 0, idx_data, idx_scale],
+                        self.ckp.log[-1, 1, idx_data, idx_scale],
+                        self.ckp.log[-1, 2, idx_data, idx_scale],
+                        self.ckp.log[-1, 3, idx_data, idx_scale],
                         best[0][idx_data, idx_scale],
                         best[1][idx_data, idx_scale] + 1
                     )
